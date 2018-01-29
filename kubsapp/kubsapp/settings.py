26955db9529ade90bd33f661483859660122fdec
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-01-30 00:20:53 +0900

    #12 http delete on allowed_host
    http

--- kubsapp/kubsapp/settings.py
+++ kubsapp/kubsapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '_72k5%1b!_wwa&f%zh2+ud!(1bvc0#--0vl1qxy)q-wl*3-lqc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://ec2-52-79-164-45.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-52-79-164-45.ap-northeast-2.compute.amazonaws.com']
 
 # Application definition
 
